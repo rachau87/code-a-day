acbc5368cd430dc5919c14b45bf88a836af5a6bd
rachau87 <rachau87@gmail.com>
2020-09-10 08:22:08 -0400

    added code for code of the day

---
+++ src/10Sep2020.ts
@@ -0,0 +1,31 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+let numbers = [10, 15, 3, 7];
+let number = 17;
+let result = false;
+
+// first attempt; works but not super efficient 
+/*numbers.forEach((num) => {
+    numbers.forEach((num2) => {
+        if ((num + num2) == number) {
+            result = true;
+        }
+    })
+})*/
+
+// seems silly that array.foreach() doesnt have a break
+// setting x = to i makes it to where you wont search the numbers that have already been used
+for (let i = 0; i < numbers.length; i++) {
+    for (let x = i; x < numbers.length; x++) {
+        if ((numbers[i] + numbers[x]) == number) {
+            result = true;
+            break;
+        }
+    }
+    if(result) {
+        break;
+    }
+}
+console.log(result);
+
+
